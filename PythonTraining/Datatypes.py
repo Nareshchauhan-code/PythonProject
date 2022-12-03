1e2bf25802388a1b254071347a288ef723e10f1c
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-12-03 17:58:18 +0530

    Data Type

---
+++ PythonTraining/Datatypes.py
@@ -0,0 +1,23 @@
+a = 5
+print(a)
+print(type(a))
+
+b = 5.2
+print(b)
+print(type(b))
+
+c = "Naresh"
+print(c)
+print(type(c))
+
+list1 = [8, 3, [10, 20], ["Naresh", "Chauhan"]]
+print(list1)
+print(type(list1))
+
+tuple1 = (("Bird", "Crow"), ("Loin", "Tiger"))
+print(tuple1)
+print(type(tuple1))
+
+dict1 = {"Name": "Naresh", "Wife": "Soina", "Son1": "Tarush", "Son2": "Manav"}
+print(dict1)
+print(type(dict1))
