7a6906ca04d86ee05119f9ad79c915f4fcf3fb81
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-12-07 17:37:25 +0530

    Input demo

---
+++ PythonTraining/InputDemo.py
@@ -0,0 +1,7 @@
+a = input("Please Enter Your name : ")
+print("My Name is : ", a)
+
+x = input("Please Enter the First value :")
+y = input("Please Enter the Second value :")
+
+print("The Total of both number is : ", int(x)+int(y))
