fd9103887cf1f185c20e0ccd9db4200578f22d2c
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-12-14 12:56:11 +0530

    String

---
+++ PythonTraining/StringMethodsDemo.py
@@ -0,0 +1,20 @@
+# String are immutable
+
+a = "Naresh Chauhan !!!"
+
+print(a.upper())
+print(a.lower())
+print(a.replace("Naresh", "John"))
+print(a.strip("!"))
+
+b = "Naresh Naresh"
+
+print(b.count("Naresh"))
+
+print(b.endswith("Naresh"))
+
+c = "Naresh Naresh 's is  's is"
+
+print(c.find("is"))
+
+print(c.isalnum())
---
+++ PythonTraining/StringsSlicing12.py
@@ -0,0 +1,5 @@
+name = "Naresh Chauhan"
+
+print(len(name))
+
+print(name[0:6])
