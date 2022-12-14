ef7b3b08a2e3759bd7bb75f7f60886186a82e5d0
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-12-14 13:50:15 +0530

    Case

---
+++ PythonTraining/MatchCaseStatements.py
@@ -0,0 +1,12 @@
+x = int(input("Please Enter the value : "))
+
+match x:
+    case 0:
+        print("X is Zero: ", x)
+
+    case 4:
+        print("X is four: ", x)
+
+    case _:
+        print("X : ",x)
+
