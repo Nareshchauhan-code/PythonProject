5bc1fa33c1e6543efcd84e6ca9610cae9e1f763c
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-12-14 21:54:32 +0530

    Loop

---
+++ PythonTraining/LoopsinPython.py
@@ -0,0 +1,26 @@
+# String
+name = "Naresh Chauhan"
+
+for i in name:
+    print(i)
+
+# List
+
+colors = ["Red", "Yellow", "Green"]
+
+for color in colors:
+
+    print(color)
+    for char in color:
+        print(char)
+
+# Range
+
+for k in range(10):
+    print(k + 1)
+
+for k in range(1, 11):
+    print(k)
+
+for k in range(1, 11, 2):
+    print(k)
