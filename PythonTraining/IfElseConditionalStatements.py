1ad895b2789ca2c73a4e72f661a4116cfbd800fe
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-12-14 13:30:35 +0530

    elif

--- PythonTraining/IfElseConditionalStatements.py
+++ PythonTraining/IfElseConditionalStatements.py
@@ -2,6 +2,10 @@ a = int(input("Enter your age: "))
 
 print("Your age is : ", a)
 
+print(a > 18)
+print(a < 18)
+print(a == 18)
+print(a != 18)
 if a > 18:
     print("You can drive")
 else:
---
+++ PythonTraining/IfElseConditionalStatements1.py
@@ -0,0 +1,9 @@
+a = int(input("Enter Number: "))
+
+if a > 0:
+    print("Number is Positive: ", a)
+elif a == 0:
+    print("Number is Zero: ", a)
+
+else:
+    print("Number is Negative: ", a)
