e5ff383d7699a37fd28caad6314021a981edb607
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-12-16 11:39:12 +0530

    While Loops

---
+++ PythonTraining/WhileLoops.py
@@ -0,0 +1,27 @@
+# Condition one
+print("Condition one")
+i = 0
+while i <= 30:
+    print(i)
+    i = i + 1
+
+print("Condition Two")
+# Condition Two
+i = 0
+while i < 30:
+    print(i)
+    i = i + 1
+
+print("Condition Three")
+# Condition Three
+i = 30
+while i >= 0:
+    print(i)
+    i = i - 1
+
+print("Condition Four")
+# Condition Four
+i = 30
+while i > 0:
+    print(i)
+    i = i - 1
