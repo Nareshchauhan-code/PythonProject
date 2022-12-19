bbac2d7344614e9bfc905722f1ad5018e8a3c350
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-12-19 18:05:25 +0530

    Operator

---
+++ PythonTraining/OperatorDemo.py
@@ -0,0 +1,14 @@
+summed = 2 + 3
+print(summed)
+difference = 10 - 8
+print(difference)
+divided = 1000 / 10
+print(divided)
+product = 7 * 5
+print(product)
+exponent = 3 ** 8
+print(exponent)
+floored = 10 // 3
+print(floored)
+mod = 17 % 15
+print(mod)  # could have used 3, 5, or 15 to get 2 as a result
