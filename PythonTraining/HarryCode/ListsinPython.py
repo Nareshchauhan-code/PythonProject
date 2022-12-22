ea9f66eda9aef2ec829df70ffa1cf02901a643b3
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-12-22 20:27:58 +0530

    List

---
+++ PythonTraining/HarryCode/ListsinPython.py
@@ -0,0 +1,16 @@
+marks = [1, 3, 6, 7, 8, 10]
+
+print(marks)
+print(type(marks))
+
+
+marks = [1, 3, 6, 7, 8, "Naresh", True]
+
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[4])
+print(marks[5])
+print(marks[6])
+
+
