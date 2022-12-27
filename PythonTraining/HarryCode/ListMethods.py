a3ffc042cfe0e2625aa456d4aa8dfe434045d282
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-12-27 22:16:00 +0530

    List Method

---
+++ PythonTraining/HarryCode/ListMethods.py
@@ -0,0 +1,18 @@
+listMethods = [1, 10, 23, 2, 5, 2]
+print(listMethods)
+
+listMethods.append(100)
+print(listMethods)
+
+listMethods.sort()
+print(listMethods)
+
+listMethods.sort(reverse=True)
+print(listMethods)
+
+listMethods.reverse()
+print(listMethods)
+
+print(listMethods.index(2))
+
+print(listMethods.count(2))
