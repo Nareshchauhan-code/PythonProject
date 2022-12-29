ac489c2c82e15f3fea78ecdca601d7119a83eb3e
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2022-12-29 18:47:29 +0530

    List methods

--- PythonTraining/HarryCode/ListMethods.py
+++ PythonTraining/HarryCode/ListMethods.py
@@ -16,3 +16,14 @@ print(listMethods)
 print(listMethods.index(2))
 
 print(listMethods.count(2))
+# Copy method
+m = listMethods.copy()
+m[0] = 1200
+print(m)
+
+# extend method
+
+s = [100, 200, 300, 400]
+
+listMethods.extend(m)
+print(listMethods)
---
+++ PythonTraining/HarryCode/f-stringsMethods.py
@@ -0,0 +1,6 @@
+letter = "my name is {1} and I am from {0}"
+name = "Naresh"
+country = "India"
+
+# print(letter.format(name, country))
+print(letter.format(country, name))
