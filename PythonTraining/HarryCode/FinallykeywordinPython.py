23a5ea37a3bc57139051abcf0be15b341037ed8f
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-01-03 20:37:27 +0530

    Finally keyword in Python

---
+++ PythonTraining/HarryCode/FinallykeywordinPython.py
@@ -0,0 +1,15 @@
+def func():
+    try:
+        l = [1, 3, 5, 67, 8]
+
+        i = int(input("Enter the Index: "))
+        print(l[i])
+        return 1
+    except:
+        print("Some Error accord")
+        return 0
+    finally:
+        print("I am always executed")
+
+        x = func()
+    print(x)
