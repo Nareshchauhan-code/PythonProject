ce22be243c667ceda1314a9b4dab594252104881
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-03-25 19:34:42 +0530

    type Casting

--- PythonTraining/CodewithHarry/PrintFun.py
+++ PythonTraining/CodewithHarry/PrintFun.py
@@ -1,1 +1,29 @@
 print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+''' 
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+'''
+# Variables in Python
+
+a = 3
+print(3)
+
+b = 1.3
+print(b)
+
+a = 2.7
+print(a)
+
+c = "Naresh"
+print(c)
+
+d = None
+print(d)
---
+++ PythonTraining/CodewithHarry/TypeCasting.py
@@ -0,0 +1,6 @@
+a = "2"
+
+print(int(a) + 4)
+
+b = 3
+print(3 + 1)
