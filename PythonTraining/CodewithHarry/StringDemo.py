5a4ca43e0f9844f35e8341427791aa85a84fc719
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-04-04 15:52:41 +0530

    UserInput

--- PythonTraining/CodewithHarry/StringDemo.py
+++ PythonTraining/CodewithHarry/StringDemo.py
@@ -6,3 +6,6 @@ print(name)
 
 print(name[0:4])
 print(name[1:6])
+print(name.upper())
+print(name.lower())
+print(name.count('a'))
---
+++ PythonTraining/CodewithHarry/UserInput.py
@@ -0,0 +1,9 @@
+name = input("Enter your name : ")
+
+print(name)
+
+num = input("Enter your number : ")
+
+print(type(num))
+
+print(int(num) + 6)
