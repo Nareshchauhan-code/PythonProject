dfcd268bf17dc1266bd8eab1ca5b9da25339635c
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-04-04 17:18:44 +0530

    FunctioinPython

---
+++ PythonTraining/CodewithHarry/FunctioinPython.py
@@ -0,0 +1,12 @@
+def geeting():
+    print("Hello ")
+
+
+geeting()
+
+
+def studentData(name, age):
+    print(f"The name of Student is {name} and age of student is {age}")
+
+
+studentData("Tarush", 7)
