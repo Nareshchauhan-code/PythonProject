b33e4de9707b2af3419aae4869884bf663ede06f
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-04-04 14:31:13 +0530

    Operator

--- PythonTraining/CodewithHarry/Operatiors.py
+++ PythonTraining/CodewithHarry/Operatiors.py
@@ -1,7 +1,10 @@
 num1 = 10
-num2 = 2
+num2 = 3
 
 print("num1 + num1 is :", num1 + num2)
 print("num1 - num1 is :", num1 - num2)
 print("num1 * num1 is :", num1 * num2)
 print("num1 / num1 is :", num1 / num2)
+print("num1 / num1 is :", num1 // num2)
+print("num1 / num1 is :", num1 % num2)
+print("num1 / num1 is :", num1 ** num2)
