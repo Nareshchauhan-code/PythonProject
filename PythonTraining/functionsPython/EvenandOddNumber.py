838e95f420ba6e86bcfd3c595e9f1c5a1e545b57
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-04-06 01:04:39 +0530

    WhileLoop

---
+++ PythonTraining/functionsPython/EvenandOddNumber.py
@@ -0,0 +1,8 @@
+num = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+for i in num:
+    if i % 2 == 0:
+        print(f"{i}: Even number")
+
+    else:
+        print(f"{i}: Odd number")
---
+++ PythonTraining/functionsPython/WhileLoop.py
@@ -0,0 +1,9 @@
+password = ""
+
+while password != "Correct Password":
+    password = input("Please Enter the  Password :")
+    if password != "Correct Password":
+        print("Please enter the correct password :")
+
+    else:
+        print("Wellcome to site")
