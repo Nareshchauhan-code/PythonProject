42dd2f731c2f8cd4357ba2d8aacf621385692651
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-04-24 18:06:37 +0530

    Loops

--- NaveenPython/ForLoopDemo.py
+++ NaveenPython/ForLoopDemo.py
@@ -1,6 +1,14 @@
 for i in range(1, 11):
     print(i)
 
+for i in range(1, 11,2):
+        print(i)
+
+# Reverse Order
+print("$$$$$$$$$$$$$$$$$$$$4")
+for i in range(20, 1, -1):
+    print(i)
+
 print("############################")
 list = [20, 1, "naresh"]
 
